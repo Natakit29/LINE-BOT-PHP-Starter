7f61a76d0de7a42c5cd7a7e5645553378a047668
rattaphoom <ruttapoom_ta@hotmail.com>
2018-07-15 22:11:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 <body onload="getLocation()">
 
 <p id="demo"></p>
-
+<form accept-charset="UTF-8" action="" class="form-inline"  method="post">
 <script>
+    
 var x = document.getElementById("demo");
 
 function getLocation() {
@@ -22,18 +23,7 @@ function showPosition(position) {
 }
 </script>
 
-    <form accept-charset="UTF-8" action="" class="form-inline"  method="post">
-  <table width="340" height="82" border="0">
-    <tr>
-      <td><div align="right">หมุ่บ้าน :</div></td>
-      <td><input   name="home_village_name" placeholder="ชื่อหมู่บ้าน"  type="text" id="home_village_name"/></td>
-    </tr>
-    <tr>
-      <td>&nbsp;</td>
-      <td><input name="commit" type="submit" value="เพิ่ม" id="commit"/>
-      <input  name="commit2" type="reset" value="ยกเลิก"/></td>
-    </tr>
-  </table>
+ <input name="commit" type="submit" value="OK" id="commit"/>
 </form>
     
     
